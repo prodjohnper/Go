fa8e5439259edc14c29441a4c80a54d09e8b0caa
prodjohnper <JonathanPerez9743@gmail.com>
2024-08-09 14:52:25 -0400

    Update - Implemented loop, save to file function and error handling as well as code refactoring

--- 02-profit_calculator/profit_calculator.go
+++ 02-profit_calculator/profit_calculator.go
@@ -1,35 +1,26 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
-func main() {
-	revenue := getUserInput("Revenue: ")   // Revenue
-	expenses := getUserInput("Expenses: ") // Expenses
-	taxRate := getUserInput("Tax rate: ")  // Tax rate
-
-	// Print divider
-	fmt.Println(`- - - - - - - - - - - - - - - -`)
-
-	// Calculate earnings before/after tax and ratio
-	earningsBeforeTax, earningsAfterTax, ratio :=
-		calculateFinancials(revenue, expenses, taxRate)
-
-	// EBT, profit and ratio calculation results
-	fmt.Printf("EBT: %.2f\n", earningsBeforeTax)
-	fmt.Printf("Profit: %.2f\n", earningsAfterTax)
-	fmt.Printf("Ratio: %.2f\n", ratio)
-}
+// File name
+const resultsFile string = "results.txt"
 
 // Function that gets user input and sets pointer to respective variable
-func getUserInput(text string) float64 {
+func getUserInput(text string) (float64, error) {
 	var userInput float64
 
 	fmt.Print(text)
 	fmt.Scan(&userInput)
 
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("Invalid amount, try again!")
+	}
+
+	return userInput, nil
 }
 
 // Function that calculates EBT, profit and ratio
@@ -39,4 +30,62 @@ func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, 
 	ratio := earningsBeforeTax / earningsAfterTax
 
 	return earningsBeforeTax, earningsAfterTax, ratio
+}
+
+// Function to save and update calculations results into file
+func writeBalanceToFile(earningsBeforeTax, earningsAfterTax, ratio float64) {
+	data := fmt.Sprintf(
+		"Ebt: %.2f\nProfit: %.2f\nRatio: %.2f\n", earningsBeforeTax, earningsAfterTax, ratio)
+
+	os.WriteFile(resultsFile, []byte(data), 0644)
+}
+
+// Function to handle errors
+func validateData(err error) bool {
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	println("Welcome!")
+	// Print divider
+	fmt.Println(`- - - - - - - - - - - - - - - -`)
+
+	for {
+		revenue, err := getUserInput("Revenue: ") // Revenue
+		if !validateData(err) {
+			continue
+		}
+
+		expenses, err := getUserInput("Expenses: ") // Expenses
+		if !validateData(err) {
+			continue
+		}
+
+		taxRate, err := getUserInput("Tax rate: ") // Tax rate
+		if !validateData(err) {
+			continue
+		}
+
+		// Print divider
+		fmt.Println(`- - - - - - - - - - - - - - - -`)
+
+		// Calculate earnings before/after tax and ratio
+		earningsBeforeTax, earningsAfterTax, ratio :=
+			calculateFinancials(revenue, expenses, taxRate)
+
+		writeBalanceToFile(earningsBeforeTax, earningsAfterTax, ratio)
+
+		// EBT, profit and ratio calculation results
+		fmt.Printf("EBT: %.2f\n", earningsBeforeTax)
+		fmt.Printf("Profit: %.2f\n", earningsAfterTax)
+		fmt.Printf("Ratio: %.2f\n", ratio)
+
+		// Print divider
+		fmt.Println(`- - - - - - - - - - - - - - - -`)
+	}
 }
